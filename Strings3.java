34f4d17e3dd4dcb6fa2c297a03b18b5634587980
sushant kumar raja <105747451+Sushantsingh1311@users.noreply.github.com>
2022-07-20 13:43:22 +0530

    Add files via upload

---
+++ Strings3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Strings3 {
+    public static void main(String[] args) {
+      //substring
+    //   String sentance="my name is Sushant";
+    //   String name=sentance.substring(11, sentance.length());
+    //   System.out.println(name);
+       
+        //  String sentance="SushantSingh";
+        //  String name=sentance.substring(7);
+        //  System.out.println(name);
+
+         String sentance="SushantSingh";
+         String name=sentance.substring(0,7);
+         System.out.println(name);
+
+
+    }
+    
+}
