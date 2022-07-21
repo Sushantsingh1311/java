062dc200aa7354834aafd0e805838ed5e64f2119
sushant kumar raja <105747451+Sushantsingh1311@users.noreply.github.com>
2022-07-21 11:59:55 +0530

    Add files via upload

---
+++ Strings4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Strings4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size=sc.nextInt();
+
+        String array[]=new String[size];
+        int totlength=0;
+
+        for(int i=0; i<size; i++){
+        array[i]=sc.next();
+        totlength +=array[i].length();
+        }
+        System.out.println(totlength);
+
+        
+    
+    
+
+    }
+    
+}
